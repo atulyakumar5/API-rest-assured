633818c4934edd2221499e2996458ed8aeece7b7
Atulya Kumar <atulyakumar5@gmail.com>
2021-11-11 01:29:06 +0530

    first commit

---
+++ src/main/java/RestAssuredProject3/RestAssuredProject3/App.java
@@ -0,0 +1,13 @@
+package RestAssuredProject3.RestAssuredProject3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
